6f76e1c74dc2df0c00ec5cac09097a6cc041c023
djkoser <koser.david@gmail.com>
2021-03-15 23:57:53 -0500

    Notice and fix key issues in figures upon reference to code

--- bookist/src/Components/BookList.js
+++ bookist/src/Components/BookList.js
@@ -2,7 +2,7 @@ import React from 'react';
 //books state and addToShelf(id)
 export default function BookList (props) {
   let bookRender = props.books.map(e=>(
-    <figure>
+    <figure key={"figureKey"+e.id}>
       <img onClick ={event=>props.addToShelf(event.target.attributes.id.value)} width='200px' id={e.id} alt={e.title} src={e.img}></img>
       <figcaption>{e.title}</figcaption>
       <figcaption>{e.author}</figcaption> 
